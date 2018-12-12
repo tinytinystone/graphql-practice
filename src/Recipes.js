882d27bc56ae7e5f7e43e950f00db5b4df770c2c
Hong Jeesoo <jeesoo.hong@gmail.com>
2018-12-12 17:03:20 +0900

    제어되는 컴포넌트와 graphql 연결하기

---
+++ src/Recipes.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const recipesQuery = gql`
+  query recipes($vegetarian: Boolean!) {
+    recipes(vegetarian: $vegetarian) {
+      id
+      title
+    }
+  }
+`;
+
+export default class recipes extends Component {
+  state = {
+    vegetarian: false
+  };
+  updateVegetarian = ({ target: { checked } }) => {
+    this.setState({
+      vegetarian: checked
+    });
+  };
+  render() {
+    const { vegetarian } = this.state;
+    return (
+      <React.Fragment>
+        <label>
+          <input
+            type="checkbox"
+            checked={vegetarian}
+            onChange={this.updateVegetarian}
+          />
+          <span>vegetarian</span>
+        </label>
+        <Query query={recipesQuery} variables={{ vegetarian: vegetarian }}>
+          {({ data, loading, error }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Something went wrong.</p>;
+            return (
+              <ul>
+                {data.recipes.map(({ id, title }) => (
+                  <li key={id}>{title}</li>
+                ))}
+              </ul>
+            );
+          }}
+        </Query>
+      </React.Fragment>
+    );
+  }
+}
